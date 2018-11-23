c4ce8be2aaf7c99a35cf7ad72515336b7769c1dc
Sachin Gupta <sachin.gupta192160@gmail.com>
2018-11-24 01:47:55 +0530

    added readonly

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -8,34 +8,40 @@ export class FormService {
 
 	constructor(private formBuilder: FormBuilder) { }
 
-	public createFormFromObject(form: FormGroup, object: any) {
+	public createFormFromObject(form: FormGroup, object: any, readOnly: boolean = false) {
 		Object.keys(object).forEach(key => {
 			if (Array.isArray(object[key])) {
 				form.addControl(key, this.formBuilder.array([]));
 				if (object[key].length != 0) {
-					this.createFormFromArray(form.controls[key] as FormArray, object[key]);
+					this.createFormFromArray(form.controls[key] as FormArray, object[key], readOnly);
 				}
 			}
 			else if (typeof object[key] == "object") {
 				form.addControl(key, this.formBuilder.group({}));
 				this.createFormFromObject(form.controls[key] as FormGroup, object[key]);
+				if (readOnly){
+					form.get(key).disable();
+				}
 			} else {
 				form.addControl(key, this.formBuilder.control(object[key]));
+				if (readOnly){
+					form.get(key).disable();
+				}
 			}
 		});
 	}
 
-	public createFormFromArray(form: FormArray, object: any[]) {
+	public createFormFromArray(form: FormArray, object: any[], readOnly: boolean = false) {
 		object.forEach(obj => {
 			let temp = this.formBuilder.group({});
 			form.push(temp);
-			this.createFormFromObject(temp, obj);
+			this.createFormFromObject(temp, obj,readOnly);
 		});
 	}
 
-	public addControlFromObject(form: FormArray, object: any, index: number = -1) {
+	public addControlFromObject(form: FormArray, object: any,readOnly:boolean =  false, index: number = -1) {
 		let tempForm = this.formBuilder.group({});
-		this.createFormFromObject(tempForm, object);
+		this.createFormFromObject(tempForm, object, readOnly);
 		if (index != -1) {
 			form.insert(index+1, tempForm);
 		} else {
